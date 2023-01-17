cbf8de2563a3c653337304e4de8cdf584e7cac71
vishalbhogate <vishal.bhogate07@gmail.com>
2023-01-17 11:06:08 +1100

    Added san to test

--- alb-listener-certificates.tf
+++ alb-listener-certificates.tf
@@ -4,6 +4,12 @@ resource "aws_lb_listener_certificate" "alb" {
   certificate_arn = var.extra_certificate_arns[count.index]
 }
 
+resource "aws_lb_listener_certificate" "alb_test" {
+  count           = var.alb ? length(var.extra_certificate_arns) : 0
+  listener_arn    = element(aws_lb_listener.ecs_test_https.*.arn, 0)
+  certificate_arn = var.extra_certificate_arns[count.index]
+}
+
 resource "aws_lb_listener_certificate" "alb_internal" {
   count           = var.alb_internal ? length(var.extra_certificate_arns) : 0
   listener_arn    = element(aws_lb_listener.ecs_https_internal.*.arn, 0)
